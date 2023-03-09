0b7c7e3e428f1510e0dee19472518f3153966dfe
Cameron Warner <cameron.w.warner@gmail.vom>
2023-03-08 19:50:03 -0500

    Initial Commit

---
+++ Gone Fishin'/Gone_Fishin_App.swift
@@ -0,0 +1,17 @@
+//
+//  Gone_Fishin_App.swift
+//  Gone Fishin'
+//
+//  Created by Cameron Warner on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Gone_Fishin_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
