770ddc438a074e639b5b74b3aa78a1cfa20c1610
Cameron Warner <cameron.w.warner@gmail.vom>
2023-06-23 17:43:51 -0400

    created haptic feedback while scrolling through poems; feedback from Apple final presentations

--- Gone Fishin'/Vol1Poems.swift
+++ Gone Fishin'/Vol1Poems.swift
@@ -1,26 +1,45 @@
 
 import SwiftUI
 import Pages
+import Haptics
 
 struct Vol1Poems: View {
+        //Haptic Feedback
+    @Environment(\.scenePhase) var scenePhase
+
+
     @State var poemIndex1: Int = 0
 
     var body: some View {
         ZStack{
             Pages(currentPage: $poemIndex1, navigationOrientation: .vertical, transitionStyle: .scroll, wrap: false) {
                 HowToFishView()
+                    .triggersHapticFeedbackWhenAppear()
+
                 BaitingView()
+                    .triggersHapticFeedbackWhenAppear()
+
                 WadingView()
+                    .triggersHapticFeedbackWhenAppear()
+
                 GoneFishin_View()
+                    .triggersHapticFeedbackWhenAppear()
+
                 BigFishSmallPondView()
+                    .triggersHapticFeedbackWhenAppear()
+
                 HookedView()
+                    .triggersHapticFeedbackWhenAppear()
+
             }// End of Table of Contents Views
             .edgesIgnoringSafeArea(.all)
+            .triggersHapticFeedbackWhenAppear()
             ScrollBlinkingArrowsUpDownView()
                 .foregroundColor(.blue)
                 .padding(.bottom, 720)
                 .padding(.trailing, 290)
         }// End of ZStack
+        .triggersHapticFeedbackWhenAppear()
     }
 }
 
